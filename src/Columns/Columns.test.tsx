ed12b4aeb10ee854d2c78fa78529bc5144496374
Sean Cicmanec <Sean.Cicmanec@interstates.com>
2022-02-24 08:20:43 -0600

    Added tests for the Columns component

--- src/Columns/Columns.test.tsx
+++ src/Columns/Columns.test.tsx
@@ -22,5 +22,50 @@ describe("Columns Component", () => {
     expect(component).toHaveTextContent(expected);
   });
 
+
+  it("should have class 'columns'", () => {
+    const expected = "columns";
+    const { getByTestId } = renderComponent({});
+
+    const component = getByTestId("Columns");
+
+    expect(component).toHaveClass(expected);
+  });
+
+  it("should always render as a div tag", () => {
+    const { getByTestId } = renderComponent({});
+
+    const component = getByTestId("Columns");
+
+    expect(component.tagName).toMatch(/div/i);
+  })
+
+  it("should have gap option of 1", () => {
+    const expected: ColumnsProps["gap"] = "is-1";
+    const { getByTestId } = renderComponent({ gap: expected });
+
+    const component = getByTestId("Columns");
+
+    expect(component).toHaveClass(expected);
+  });
+
+  it("should have mobile responsiveness", () => {
+    const expected: ColumnsProps["responsive"] = "is-mobile";
+    const { getByTestId } = renderComponent({ responsive: expected });
+
+    const component = getByTestId("Columns");
+
+    expect(component).toHaveClass(expected);
+  });
+
+  it("should vertically aligned", () => {
+    const expected: ColumnsProps["options"] = "is-vcentered";
+    const { getByTestId } = renderComponent({ options: expected });
+
+    const component = getByTestId("Columns");
+
+    expect(component).toHaveClass(expected);
+  });
+
   testBulmaProps("Columns", renderComponent);
 });
