b9f4f2834420466d60fdb7ceea2d4a37bdc2fe2b
Sean Cicmanec <Sean.Cicmanec@interstates.com>
2022-02-24 08:45:01 -0600

    Added tests for the Container component

--- src/Container/Container.test.tsx
+++ src/Container/Container.test.tsx
@@ -18,5 +18,49 @@ describe("Container Component", () => {
     expect(component).toHaveTextContent(expected);
   });
 
+  it("should have class 'container'", () => {
+    const expected = "container";
+    const { getByTestId } = renderComponent({});
+
+    const component = getByTestId("Container");
+
+    expect(component).toHaveClass(expected);
+  });
+
+  it("should always render as a div tag", () => {
+    const { getByTestId } = renderComponent({});
+
+    const component = getByTestId("Container");
+
+    expect(component.tagName).toMatch(/div/i);
+  })
+
+  it("should be fluid container", () => {
+    const expected: ContainerProps["isFluid"] = true;
+    const { getByTestId } = renderComponent({ isFluid: expected });
+
+    const component = getByTestId("Container");
+
+    expect(component).toHaveClass("is-fluid");
+  });
+
+  it("should not be fluid container", () => {
+    const expected: ContainerProps["isFluid"] = false;
+    const { getByTestId } = renderComponent({ isFluid: expected });
+
+    const component = getByTestId("Container");
+
+    expect(component).not.toHaveClass("is-fluid");
+  });
+
+  it("should be widescreen container", () => {
+    const expected: ContainerProps["breakpoint"] = "is-widescreen";
+    const { getByTestId } = renderComponent({ breakpoint: expected });
+
+    const component = getByTestId("Container");
+
+    expect(component).toHaveClass(expected);
+  });
+
   testBulmaProps("Container", renderComponent);
 });
