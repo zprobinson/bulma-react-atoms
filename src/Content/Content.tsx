358542ce217ff09d9cd5c959ab93bdb7a3d0bd93
Zach Robinson <zach.robinson@interstates.com>
2022-02-28 09:34:18 -0600

    updated content to null coalesce the incoming props

--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -3,7 +3,7 @@ import { useInnerBulmaProps } from "../utilities/propUtilities";
 import { ContentProps } from "./Content.types";
 
 const Content: React.FC<ContentProps> = ({ size, ...props }) => {
-  const { classNames, rest } = useInnerBulmaProps(props, size);
+  const { classNames, rest } = useInnerBulmaProps(props, size ?? "");
   return (
     <div
       data-testid="Content"
