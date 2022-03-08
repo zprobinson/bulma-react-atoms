7626fe546070784a3614d42975211d68812f28df
Zach Robinson <zach.robinson@interstates.com>
2022-03-07 19:59:42 -0600

    removed tabsLink because calling code can provide their own children in a more maintainable matter than a polymorphic component

--- src/Tabs/Tabs.test.tsx
+++ src/Tabs/Tabs.test.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 
 import Tabs from './Tabs';
-import TabsLink from './TabsLink';
 import TabsList from './TabsList';
 import TabsListItem from './TabsListItem';
-import {
-    TabsProps,
-    TabsLinkProps,
-    TabsListProps,
-    TabsListItemProps,
-} from './Tabs.types';
+import { TabsProps, TabsListProps, TabsListItemProps } from './Tabs.types';
 import { testBulmaProps } from '../bulmaTests/bulmaTests';
 
 describe('Tabs Component', () => {
@@ -26,40 +20,6 @@ describe('Tabs Component', () => {
     });
 
     testBulmaProps('Tabs', renderComponent);
-});
-
-describe('Tabs Link Component', () => {
-    const renderComponent = (props: TabsLinkProps) =>
-        render(<TabsLink {...props} />);
-
-    it('should render children correctly', () => {
-        const expected = 'harvey was here';
-        renderComponent({
-            children: expected,
-            isActive: true,
-            textColor: 'has-text-danger',
-            innerListItemProps: {
-                className: 'foo-bar',
-            },
-        });
-
-        const component = screen.getByTestId('TabsLink');
-
-        expect(component).toHaveTextContent(expected);
-    });
-
-    it('should recognize a single click', () => {
-        const onClick = jest.fn();
-        renderComponent({ onClick });
-
-        const component = screen.getByTestId('TabsLinkAnchor');
-        fireEvent.click(component);
-
-        expect(onClick).toHaveBeenCalledTimes(1);
-    });
-
-    // in this component, bulma helpers are applied to the inner anchor tag.
-    testBulmaProps('TabsLinkAnchor', renderComponent);
 });
 
 describe('Tabs List Component', () => {
