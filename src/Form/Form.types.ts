c238daa2aec7c275b207b3532746146be0c55e76
Zach Robinson <zach.robinson@interstates.com>
2022-05-19 12:25:21 -0500

    Add boolean flag to allow the field to determine whether it is a horizontal field or vertical field.

--- src/Form/Form.types.ts
+++ src/Form/Form.types.ts
@@ -14,6 +14,7 @@ import {
 
 export type FormFieldContextType = {
     size?: Size | undefined;
+    isHorizontal?: boolean | undefined;
 };
 
 export type FormProps = BulmaComponentPropsWithRef<'form'>;
